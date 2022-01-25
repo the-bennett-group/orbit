c294230902493b3c7b98d721bbb2798abb56c6b9
peaceheis <69596656+peaceheis@users.noreply.github.com>
2022-01-25 15:20:57 -0600

    Blackwood Rotates Now

--- src/main/java/bennett/orbit/blocks/OrbitBlocks.java
+++ src/main/java/bennett/orbit/blocks/OrbitBlocks.java
@@ -1,10 +1,8 @@
 package bennett.orbit.blocks;
 
-import bennett.orbit.fluid.AcidFluid;
 import net.minecraft.core.Registry;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.FlowingFluid;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import bennett.orbit.Orbit;
 import bennett.orbit.fluid.OrbitFluids;
@@ -21,9 +19,9 @@ public final class OrbitBlocks {
 	public static void initialize() {
 		ACID = register(new LiquidBlock(OrbitFluids.SOURCE_ACID, FabricBlockSettings.copyOf(Blocks.WATER).luminance(9)){}, "acid");
 		SALT_BLOCK = register(new GlassBlock(BlockBehaviour.Properties.of(Material.GLASS).strength(0.3f).sound(SoundType.GLASS)), "salt_block");
-		BLACKWOOD_LOG = register(new Block(FabricBlockSettings.of(Material.WOOD).strength(2.0f)), "blackwood_log");
-		STRIPPED_BLACKWOOD_LOG = register(new Block(FabricBlockSettings.of(Material.WOOD).strength(2.0f)), "stripped_blackwood_log");
-		BLACKWOOD_PLANKS = register(new Block(FabricBlockSettings.of(Material.WOOD).strength(2.0f)), "blackwood_planks");
+		BLACKWOOD_LOG = register(new RotatedPillarBlock(FabricBlockSettings.copyOf(Blocks.OAK_LOG).strength(2.0F).sounds(SoundType.WOOD)), "blackwood_log");
+		STRIPPED_BLACKWOOD_LOG = register(new RotatedPillarBlock(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(SoundType.WOOD)), "stripped_blackwood_log");
+		BLACKWOOD_PLANKS = register(new RotatedPillarBlock(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(SoundType.WOOD)), "blackwood_planks");
 	}
 
 	public static Block register(Block block, String name) {
