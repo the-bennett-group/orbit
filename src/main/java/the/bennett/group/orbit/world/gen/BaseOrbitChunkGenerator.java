05110a76e418d18b39a005d026dbb1ebff12aa80
peaceheis <69596656+peaceheis@users.noreply.github.com>
2022-05-10 21:47:51 -0500

    Clean up Chunk Generators before custom worldgen
    Signed-off-by: peaceheis <69596656+peaceheis@users.noreply.github.com>

--- src/main/java/the/bennett/group/orbit/world/gen/BaseOrbitChunkGenerator.java
+++ src/main/java/the/bennett/group/orbit/world/gen/BaseOrbitChunkGenerator.java
@@ -4,16 +4,15 @@ import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.Registry;
-import net.minecraft.server.level.WorldGenRegion;
 import net.minecraft.world.level.LevelHeightAccessor;
 import net.minecraft.world.level.NoiseColumn;
 import net.minecraft.world.level.StructureFeatureManager;
-import net.minecraft.world.level.biome.BiomeManager;
 import net.minecraft.world.level.biome.BiomeSource;
 import net.minecraft.world.level.biome.Climate;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.ChunkGenerator;
-import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.blending.Blender;
 import net.minecraft.world.level.levelgen.structure.StructureSet;
@@ -24,36 +23,38 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
 public abstract class BaseOrbitChunkGenerator extends ChunkGenerator {
+    private long seed;
+    protected static final BlockState AIR = Blocks.AIR.defaultBlockState();
+    protected final int verticalNoiseResolution = 8;
+    protected final int horizontalNoiseResolution = 4;
+    protected final int noiseSizeX = 16 / this.horizontalNoiseResolution;
+    protected final int noiseSizeY = 256 / this.verticalNoiseResolution;
+    protected final int noiseSizeZ = 16 / this.horizontalNoiseResolution;
+
     public BaseOrbitChunkGenerator(Registry<StructureSet> registry, Optional<HolderSet<StructureSet>> optional, BiomeSource biomeSource) {
         super(registry, optional, biomeSource);
     }
 
-    protected abstract Codec<? extends ChunkGenerator> codec();
+    @Override
+    protected Codec<? extends ChunkGenerator> codec() {
+        return CODEC;
+    }
 
-    public abstract ChunkGenerator withSeed(long l);
+    @Override
+    public ChunkGenerator withSeed(long l) {
+        this.seed = l;
+        return this;
+    }
+
+    public abstract BlockState getDefaultBlock();
+
+    public abstract BlockState getDefaultFluid();
 
     @Override
     public Climate.Sampler climateSampler() {
         return null;
     }
 
-    @Override
-    public void applyCarvers(WorldGenRegion worldGenRegion, long l, BiomeManager biomeManager, StructureFeatureManager structureFeatureManager, ChunkAccess chunkAccess, GenerationStep.Carving carving) {
-
-    }
-
-    @Override
-    public void buildSurface(WorldGenRegion worldGenRegion, StructureFeatureManager structureFeatureManager, ChunkAccess chunkAccess) {
-
-    }
-
-    @Override
-    public abstract void spawnOriginalMobs(WorldGenRegion worldGenRegion);
-
-    @Override
-    public int getGenDepth() {
-        return 0;
-    }
 
     @Override
     public CompletableFuture<ChunkAccess> fillFromNoise(Executor executor, Blender blender, StructureFeatureManager structureFeatureManager, ChunkAccess chunkAccess) {
@@ -62,19 +63,35 @@ public abstract class BaseOrbitChunkGenerator extends ChunkGenerator {
         return CompletableFuture.completedFuture(chunkAccess);
     }
 
-    private void fillFromNoise(StructureFeatureManager structureFeatureManager, ChunkAccess chunkAccess) {
-        return;
+    private CompletableFuture<ChunkAccess> fillFromNoise(StructureFeatureManager structureFeatureManager, ChunkAccess chunk) {
+        BlockPos.MutableBlockPos mutableBlockPos = new BlockPos.MutableBlockPos();
+        Heightmap heightmap = chunk.getOrCreateHeightmapUnprimed(Heightmap.Types.OCEAN_FLOOR_WG);
+        Heightmap heightmap2 = chunk.getOrCreateHeightmapUnprimed(Heightmap.Types.WORLD_SURFACE_WG);
+        BlockState defaultBlock = getDefaultBlock();
+        BlockState defaultFluid = getDefaultFluid();
+        for(int i = 0; i <= 64; ++i) {
+            if (defaultBlock != null) {
+                int j = chunk.getMinBuildHeight() + i;
+                for(int k = 0; k < 16; ++k) {
+                    for(int l = 0; l < 16; ++l) {
+                        chunk.setBlockState(mutableBlockPos.set(k, j, l), defaultBlock, false);
+                        heightmap.update(k, j, l, defaultBlock);
+                        heightmap2.update(k, j, l, defaultBlock);
+                    }
+                }
+            }
+        }
+        postProcess();
+        return CompletableFuture.completedFuture(chunk);
     }
 
-    @Override
-    public int getSeaLevel() {
-        return 0;
-    }
+    public abstract CompletableFuture<ChunkAccess> postProcess();
 
     @Override
-    public int getMinY() {
-        return 0;
-    }
+    public abstract int getSeaLevel();
+
+    @Override
+    public abstract int getMinY();
 
     @Override
     public int getBaseHeight(int i, int j, Heightmap.Types types, LevelHeightAccessor levelHeightAccessor) {
@@ -82,12 +99,8 @@ public abstract class BaseOrbitChunkGenerator extends ChunkGenerator {
     }
 
     @Override
-    public NoiseColumn getBaseColumn(int i, int j, LevelHeightAccessor levelHeightAccessor) {
-        return null;
-    }
+    public abstract NoiseColumn getBaseColumn(int i, int j, LevelHeightAccessor levelHeightAccessor);
 
     @Override
-    public void addDebugScreenInfo(List<String> list, BlockPos blockPos) {
-
-    }
+    public abstract void addDebugScreenInfo(List<String> list, BlockPos blockPos);
 }
--- src/main/java/the/bennett/group/orbit/world/planets/casud/gen/CasudChunkGenerator.java
+++ src/main/java/the/bennett/group/orbit/world/planets/casud/gen/CasudChunkGenerator.java
@@ -3,7 +3,6 @@ package the.bennett.group.orbit.world.planets.casud.gen;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.server.level.WorldGenRegion;
@@ -16,61 +15,44 @@ import net.minecraft.world.level.biome.Climate;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
-import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.*;
-import net.minecraft.world.level.levelgen.blending.Blender;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureManager;
 import org.quiltmc.loader.api.QuiltLoader;
 import the.bennett.group.orbit.blocks.OrbitBlocks;
 import the.bennett.group.orbit.fluid.OrbitFluids;
 import the.bennett.group.orbit.util.SeedHolder;
+import the.bennett.group.orbit.world.gen.BaseOrbitChunkGenerator;
 import the.bennett.group.orbit.world.planets.casud.Casud;
-import the.bennett.group.orbit.world.planets.casud.biomes.CasudBiomes;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 
-public class CasudChunkGenerator extends ChunkGenerator {
+public class CasudChunkGenerator extends BaseOrbitChunkGenerator {
+    private long seed;
+    public final BiomeSource biomeSource;
+    private final WorldgenRandom worldgenRandom;
+    private final BlockState defaultBlock;
+    private final BlockState defaultFluid;
+    private final int seaLevel;
+    //private final NoiseGeneratorSettings noiseGeneratorSettings;
+    private final SurfaceSystem surfaceSystem;
+    private final LegacyRandomSource randomSource;
+    private final Aquifer.FluidPicker aquiferFluidPicker;
+
     public static final Codec<CasudChunkGenerator> CODEC = RecordCodecBuilder.create((instance) ->
             instance.group(
                             BiomeSource.CODEC.fieldOf("biome_source").forGetter((generator) -> generator.biomeSource),
                             Codec.LONG.fieldOf("seed").stable().orElseGet(SeedHolder::seed).forGetter((generator) -> generator.seed))
                     .apply(instance, instance.stable(CasudChunkGenerator::new)));
 
-    private long seed;
-    public final BiomeSource biomeSource;
-    private final WorldgenRandom worldgenRandom;
-    private final BlockState defaultBlock;
-    private final BlockState defaultFluid;
-    private static final BlockState AIR = Blocks.AIR.defaultBlockState();
-    private final int verticalNoiseResolution;
-    private final int horizontalNoiseResolution;
-    private final WorldgenRandom random;
-    private final int noiseSizeX;
-    private final int noiseSizeY;
-    private final int noiseSizeZ;
-    private final int seaLevel;
-    private final NoiseGeneratorSettings noiseGeneratorSettings;
-    private final SurfaceSystem surfaceSystem;
-    private final LegacyRandomSource randomSource;
-    private final Aquifer.FluidPicker aquiferFluidPicker;
 
-    //TODO: move init functionality into BaseOrbitChunkGenerator
     public CasudChunkGenerator(BiomeSource biomeSource, long seed) {
         super(BuiltinRegistries.STRUCTURE_SETS, Optional.empty(), biomeSource);
-        this.seed = seed;
         this.biomeSource = biomeSource;
-        this.worldgenRandom = new WorldgenRandom(new XoroshiroRandomSource(RandomSupport.seedUniquifier()));
         this.defaultBlock = OrbitBlocks.SALT_BLOCK.defaultBlockState();
         this.defaultFluid = OrbitFluids.SOURCE_ACID.defaultFluidState().createLegacyBlock();
-        this.verticalNoiseResolution = 8;
-        this.horizontalNoiseResolution = 4;
-        this.noiseSizeX = 16 / this.horizontalNoiseResolution;
-        this.noiseSizeY = 256 / this.verticalNoiseResolution;
-        this.noiseSizeZ = 16 / this.horizontalNoiseResolution;
-        this.random = new WorldgenRandom(new LegacyRandomSource(seed));
+        this.worldgenRandom = new WorldgenRandom(new LegacyRandomSource(seed));
         this.seaLevel = Casud.SEA_LEVEL;
         this.randomSource = new LegacyRandomSource(SeedHolder.seed()); //TODO: figure out if this should actually be the world seed: for now I think its fine.
         this.surfaceSystem = new SurfaceSystem(BuiltinRegistries.NOISE, this.defaultBlock, this.seaLevel, SeedHolder.seed(), WorldgenRandom.Algorithm.LEGACY);
@@ -80,14 +62,13 @@ public class CasudChunkGenerator extends ChunkGenerator {
 
 
     @Override
-    protected Codec<? extends ChunkGenerator> codec() {
-        return CODEC;
+    public BlockState getDefaultBlock() {
+        return this.defaultBlock;
     }
 
     @Override
-    public ChunkGenerator withSeed(long l) {
-        this.seed = l;
-        return this;
+    public BlockState getDefaultFluid() {
+        return this.defaultFluid;
     }
 
     @Override
@@ -101,16 +82,20 @@ public class CasudChunkGenerator extends ChunkGenerator {
 
     }
 
+    public CompletableFuture<ChunkAccess> postProcess() {
+        return new CompletableFuture<>();
+    }
+
     @Override
     public void buildSurface(WorldGenRegion region, StructureFeatureManager structures, ChunkAccess chunk) {
-        WorldGenerationContext worldGenerationContext = new WorldGenerationContext(this, region);
+        /*WorldGenerationContext worldGenerationContext = new WorldGenerationContext(this, region);
         NoiseChunk noiseChunk = chunk.getOrCreateNoiseChunk(this.router, () -> {
             return new Beardifier(structureFeatureManager, chunkAccess);
         }, noiseGeneratorSettings, this.globalFluidPicker, Blender.of(worldGenRegion));
 
         this.surfaceSystem.buildSurface(region.getBiomeManager(), region.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY), true, worldGenerationContext, noiseChunk, CasudBiomes.RULE_SOURCE);
         this.buildBedrock(chunk, random);
-
+*/
     }
 
     protected void buildBedrock(ChunkAccess chunk, WorldgenRandom random) {
@@ -137,28 +122,7 @@ public class CasudChunkGenerator extends ChunkGenerator {
         return 0;
     }
 
-    @Override
-    public CompletableFuture<ChunkAccess> fillFromNoise(Executor executor, Blender blender, StructureFeatureManager structureFeatureManager, ChunkAccess chunk) {
-        BlockPos.MutableBlockPos mutableBlockPos = new BlockPos.MutableBlockPos();
-        Heightmap heightmap = chunk.getOrCreateHeightmapUnprimed(Heightmap.Types.OCEAN_FLOOR_WG);
-        Heightmap heightmap2 = chunk.getOrCreateHeightmapUnprimed(Heightmap.Types.WORLD_SURFACE_WG);
 
-        for(int i = 0; i <= 64; ++i) {
-            BlockState blockState = defaultBlock;
-            if (blockState != null) {
-                int j = chunk.getMinBuildHeight() + i;
-                for(int k = 0; k < 16; ++k) {
-                    for(int l = 0; l < 16; ++l) {
-                        chunk.setBlockState(mutableBlockPos.set(k, j, l), blockState, false);
-                        heightmap.update(k, j, l, blockState);
-                        heightmap2.update(k, j, l, blockState);
-                    }
-                }
-            }
-        }
-
-        return CompletableFuture.completedFuture(chunk);
-    }
 
     @Override
     public void createStructures(RegistryAccess registryAccess, StructureFeatureManager structureFeatureManager, ChunkAccess chunkAccess, StructureManager structureManager, long l) {
@@ -205,5 +169,5 @@ public class CasudChunkGenerator extends ChunkGenerator {
         }
     }
 
-    void addBedrock()
+
 }
