985ad1cf2ac20216bb2f11a1fb16f00a62ba532d
peaceheis <69596656+peaceheis@users.noreply.github.com>
2022-10-26 22:12:40 -0400

    WIP Fixing Trees

--- src/main/java/the/bennett/group/orbit/blocks/BlackwoodSaplingBlock.java
+++ src/main/java/the/bennett/group/orbit/blocks/BlackwoodSaplingBlock.java
@@ -3,6 +3,7 @@ package the.bennett.group.orbit.blocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SaplingBlock;
@@ -17,6 +18,12 @@ public class BlackwoodSaplingBlock extends SaplingBlock {
 
     @Override
     public boolean mayPlaceOn(BlockState floor, BlockGetter world, BlockPos pos) {
-        return floor.getTags().anyMatch((tag) -> tag == OrbitTags.BLACKWOOD_PLACEABLE);
+        return floor.is(OrbitTags.BLACKWOOD_PLACEABLE);
+    }
+
+    @Override
+    public boolean canSurvive(BlockState state, LevelReader world, BlockPos pos) {
+        BlockPos blockPos = pos.below();
+        return this.mayPlaceOn(world.getBlockState(blockPos), world, blockPos);
     }
 }
